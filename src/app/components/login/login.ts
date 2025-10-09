b2e45e9a7dfd7f1024350b26c729ac4c40882b44
Gloria Maggioni <105869401+GloriaMaggioni@users.noreply.github.com>
2025-10-09 14:43:38 +0200

    lofin's form non funzionante

--- src/app/components/login/login.ts
+++ src/app/components/login/login.ts
@@ -1,27 +1,42 @@
 import { Component, Inject } from '@angular/core';
-import { ActivatedRoute, RouterLink, RouterModule, Routes,  } from "@angular/router";
+import { ActivatedRoute, RouterLink, RouterModule, Router,  } from "@angular/router";
 import{FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators} from "@angular/forms"
+import { error } from 'node:console';
 @Component({
   selector: 'app-login',
   imports: [
     ReactiveFormsModule,
     FormsModule,
-    RouterModule,
-    
-    
+    RouterModule, 
   ],
   templateUrl: './login.html',
   styleUrl: './login.css'
 })
 export class Login {
-   
 
-  // constructor(private route: Router){}
+  private route = Inject(ActivatedRoute);
+  form  = new FormGroup({
+    email: new FormControl ('', Validators.required),
+    password: new FormControl('', [Validators.required, Validators.minLength(8)])
+
+  })
+ private router = Inject (Router);
+
+ 
+  goToDashboard(){    //NON FUNZIONA: CAPIRE CHE COSA È SBAGLIATO: COMPARE SOLO L'ALERT SIA CHE COMPILO O MENO I CAMBI DEL
+    if(this.form.valid){
+          this.route.navigate[('/src/app/components/dashboard/dashboard.html')]
+          console.log(this.form.value)
+
+    }else if(this.form.invalid){                 
+      alert('Compilare tutti i campi obbligatori') 
+    }
+  }
 
 
   goToRegisterPage(){
-    // this.route.navigate[('/register/register.html')] //mi da errore: vedere perchè qui non funziona .Sistemare la autenticazione dei campi
+    this.route.navigate[('/register/register.html')]
+    console.log(this.goToRegisterPage())
   }
- 
 
 }
