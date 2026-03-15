852b4cd7cea62d544bc5c2de85a20d804653b0d7
Gloria Maggioni <105869401+GloriaMaggioni@users.noreply.github.com>
2026-03-15 12:46:00 +0100

    Creato newPost form con la validazione

--- src/app/components/post/post.ts
+++ src/app/components/post/post.ts
@@ -1,6 +1,7 @@
 import { Component, inject, OnInit } from '@angular/core';
 import { PostModel } from '../../models/post-model';
 import { PostService } from '../../services/post-service';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-post',
@@ -8,8 +9,16 @@ import { PostService } from '../../services/post-service';
   templateUrl: './post.html',
   styleUrl: './post.css',
 })
-export class Post implements OnInit {
+export class Post  {
   private service = inject(PostService)
+
+  private userId : string = "Id utente creata dall'autenticazione"    // TODO: COLLEGARE L'USERID ALLA AUTENTICAZIONE QUANDO VERRA' CREATA
+   
+newPostForm = new  FormGroup({
+     userName : new FormControl ('', Validators.required),
+     image: new FormControl(''),
+     postDescription: new FormControl('', Validators.required)
+});
 
   newPost : PostModel = {       // inizializza i dati del post 
     name: '',
@@ -17,23 +26,25 @@ export class Post implements OnInit {
     description: '',
     comment : []
   };
-  ngOnInit(): void {
-    
-  }
+ 
 
  
    addNewPostClick(){
-   this.service.createPost(this.newPost, this.newPost.id). subscribe({
+   this.service.createPost(this.newPost, this.userId). subscribe({         // userId provvisorio ---> collegarla all'autenticazione
     next : (data: any) =>{
-      console.log('Dati del nuovo post:', data)
+      // console.log('Dati del nuovo post:', data)
+      console.log('Dati del nuovo post inviati')
     },
-    error : (err : any) =>{       console.error('Errore nel creare il nuovo post:', err)
+    error : (err : any) =>{       
+      console.error('Errore nel creare il nuovo post:', err)
      }
    })
+
+   this.cleanForm()
  }
 
  // resetta il form
- cleanForm(){   //TODO: VEDERE DOVA VA ATTACCATO PER RESETTARE IL FORM
+ cleanForm(){   //TODO: VEDERE DOVE VA ATTACCATO PER RESETTARE IL FORM
   this.newPost = {
     name: '',
     data: '',
