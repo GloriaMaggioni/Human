3194f21a5744ba78d66ac2eb17701c018f0e03ba
Gloria Maggioni <105869401+GloriaMaggioni@users.noreply.github.com>
2026-03-26 18:59:29 +0100

    Aggiunti utenti alla homepage

--- src/app/components/homepage/homepage.component.ts
+++ src/app/components/homepage/homepage.component.ts
@@ -1,4 +1,4 @@
-import { ChangeDetectionStrategy, Component, ElementRef, model, signal, ViewChild } from '@angular/core';
+import { ChangeDetectionStrategy, Component, ElementRef, inject, Input, model, OnInit, signal, ViewChild } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { HttpClient } from '@angular/common/http';
 import { Signal } from '@angular/core';
@@ -7,20 +7,40 @@ import {provideNativeDateAdapter} from '@angular/material/core';
 import {MatDatepickerModule} from '@angular/material/datepicker';
 import { MatNativeDateModule } from '@angular/material/core';
 import { MatInputModule } from '@angular/material/input';
+import { UtentsPageComponent } from "../utents-page/utents-page.component";
+import { UsersService } from '../../services/users-service';
+import { User } from '../../models/users';
+import { AsyncPipe, SlicePipe } from '@angular/common';
 
 
 @Component({
   selector: 'app-homepage',
-  imports: [RouterModule, MatCardModule, MatDatepickerModule, MatNativeDateModule, MatInputModule],
+  imports: [RouterModule, MatCardModule, MatDatepickerModule, MatNativeDateModule, MatInputModule,RouterModule, SlicePipe],
   templateUrl: './homepage.component.html',
   styleUrl: './homepage.component.css'
 })
-export class HomepageComponent {
-  selected = model<Date | null>(null);
+export class HomepageComponent implements OnInit {
+  private userService = inject(UsersService)
+   limit : number = 30
+  currentPage : number = 1
+  @Input() offset : number = (this.currentPage - 1) * this.limit;
+  
+  users : User[] = []
+  ngOnInit(): void {
+    this.showUtents()
+  }
 
-isOpen = false;
-eventoForm(){
-  this.isOpen = !this.isOpen;
-}
+  showUtents(){
+    this.userService.getUser(1, undefined, 6)
+    this.userService.users$.subscribe({
+      next: (data: any) =>{
+        this.users = data;
+        console.log('Utenti dalla homepage', data)
+
+      }
+    })
+  }
+ 
+
 
 }
--- src/app/services/users-service.ts
+++ src/app/services/users-service.ts
@@ -15,13 +15,14 @@ export class UsersService  {
    private apiUrl : string=  'https://gorest.co.in/public/v2/users';     // api url per users
     private myToken: string = environment.GOREST_APIKEY;
    // my api token
-   private headers = new HttpHeaders({                                                                    // impostazione http headers
+   private headers = new HttpHeaders({                                  // impostazione http headers
     'Content-Type': 'application/json',
     'Authorization': 'Bearer ' + this.myToken
    })
 
-   users$ = new BehaviorSubject<User[]>([]);     // immagazzina i dati degli users
-   totalUser$ = new BehaviorSubject<number>(0)   // totale degli users
+   users$ = new BehaviorSubject<User[]>([]);                           // immagazzina i dati degli users
+   totalUser$ = new BehaviorSubject<number>(0)                        // totale degli users
+   homepageUsers$ = new BehaviorSubject<User[]>([])
 
    
    // metodo per aggiunger un nuovo user (call api)
@@ -31,9 +32,9 @@ export class UsersService  {
 
 
    // metodo per prendere i dati degli user
-   getUser(pageNumber : number = 1, searchText?: string){
+   getUser(pageNumber : number = 1, searchText?: string, perPage: number = 30){
     if(typeof searchText !== 'undefined' ){
-      const searchUserUrl : string =  `https://gorest.co.in/public/v2/users?name=${searchText}&per_page=30`
+      const searchUserUrl : string =  `https://gorest.co.in/public/v2/users?name=${searchText}&per_page=${perPage}`;
 
       this.http.get(`${searchUserUrl}`, {headers: this.headers, observe: 'response'}).subscribe({
         next : (response: any) =>{
@@ -45,7 +46,7 @@ export class UsersService  {
         }
       })
     }else {
-      this.http.get(`https://gorest.co.in/public/v2/users?page=${pageNumber}&per_page=30`, {headers: this.headers, observe: 'response'}).subscribe({
+      this.http.get(`https://gorest.co.in/public/v2/users?page=${pageNumber}&per_page=${perPage}`, {headers: this.headers, observe: 'response'}).subscribe({
       next: (response : any) =>{
         this.users$.next(response.body)
         const total = response.headers.get('X-Pagination-Total')
